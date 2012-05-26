9c4371f29b32682c31c91b7f7b6946aecb04c6ee
shubham kaushal <1shubham@gmail.com>
2012-05-26 22:09:43 +0800

    parsing completed (to do: convert strings to relevant formats and package them as a task object)

--- Parser + Regex Package/ParserTestEngine 0.1/src/ParserPackage/Parser.java
+++ Parser + Regex Package/ParserTestEngine 0.1/src/ParserPackage/Parser.java
@@ -3,6 +3,10 @@ package ParserPackage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Vector;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Parser {
 	/*
@@ -11,11 +15,28 @@ public class Parser {
 		return null;
 	}*/
 	
+	/*
+	 * make all helper functions non static
+	 */
+	private final static String RECUR_REGEX = "(?i)(weekly|monthly|yearly)";
+	private final static String LABEL_REGEX = "@((\\s+)?(\\S+(\\s+)?,))+((\\s+)?(\\S+))";
+	
+	public static String removeExtraSpaces (String s) {
+		return s.replaceAll("\\s+", " ");
+	}
+	
 	public static void main (String args[]) {
 		String inputString = null;
 		BufferedReader reader;
 		reader = new BufferedReader(new InputStreamReader(System.in));
 
+		/*
+		if ("@   sdafads     , dsfds , sdfds".matches(LABEL_REGEX))
+			System.out.println("matches");
+		else
+			System.out.println("DOES NOT match");
+		*/
+		
 		System.out.print("Input string:");
 		try {
 			inputString = reader.readLine();
@@ -24,15 +45,95 @@ public class Parser {
 			System.out.println("An unexpected error occured for your input");
 		}
 		
-		inputString = inputString.trim();
+		startParsing (inputString);
+		
+	}
+	
+	public static void startParsing(String inputS) {
+
+		inputS = inputS.trim();
+		
+		if(checkImportant(inputS)) {
+			System.out.println("IMPORTANT TASK!");
+			inputS = inputS.replace('*', '\0');
+			inputS = inputS.trim();
+		}
+		
+		String recurString = getRecurString (inputS);
+		
+		if (recurString != null)
+			System.out.println("this task is "+recurString);
+		else
+			System.out.println("this task is not recurring");
+		
+		
+		inputS = inputS.replaceFirst(RECUR_REGEX, "");
+		inputS = removeExtraSpaces(inputS);
+		inputS = inputS.trim();
+		
+		System.out.println("left over string after checking for recurring: "+inputS);
+		
+		
+		Vector<String> labelVector = getLabels(inputS);
+		
+		if(!labelVector.isEmpty()) {
+			for (int i=0; i<labelVector.size(); i++)
+				System.out.println("label "+i+": "+labelVector.get(i));
+			
+			inputS = inputS.replaceFirst(LABEL_REGEX, "");
+			System.out.println("left over string after fetching labels: "+inputS);
+		}
 		
 		TimeParser timeParser = new TimeParser();
 		//DateParser dateParser = new DateParser();
 		
-		if(timeParser.extractTime(inputString))
-			System.out.println("extracted!");
+		if(timeParser.extractTime(inputS))
+			System.out.println("time/date extracted!");
 		else
-			System.out.println("NOT extracted!");
+			System.out.println("time/date NOT extracted!");
+		
+	}
+	
+	public static boolean checkImportant (String s) {
+		if (s.startsWith("*")){
+			//s = s.replace('*', '\0');
+			//s = s.trim();
+			return true;
+		}
+		return false;
+	}
+	
+	public static String getRecurString (String s) {
+		Pattern p = Pattern.compile(RECUR_REGEX);
+		Matcher m = p.matcher(s);
+		
+		String recurString=null;
+		
+		if (m.find()) {
+			recurString = m.group();
+			//s = s.replaceFirst("(?i)(weekly|monthly|yearly)", "");
+			//s = removeExtraSpaces(s);
+		}
+		
+		return recurString;
+	}
+	
+	public static Vector<String> getLabels(String s) {
+		Pattern p = Pattern.compile(LABEL_REGEX);
+		Matcher m = p.matcher(s);
+		String labelString = null;
+		Vector<String> labelVec = null;
+		
+		if(m.find()) {
+			labelString = m.group();
+			labelString.replace('@', '\0');
+			labelString = labelString.trim();
+			
+			String[] labelArr = labelString.split("( )?,( )?");
+			labelVec = new Vector<String>(Arrays.asList(labelArr));
+		}
+		
+		return labelVec;
 	}
 
 }
